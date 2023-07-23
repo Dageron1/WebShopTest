e6c17240519fd2da78997df60f97d8d317ce0db5
Dageron <Dageron12345@mail.ru>
2023-07-23 21:46:12 +0200

    Up

--- WebShop.DataAccess/Data/ApplicationDbContext.cs
+++ WebShop.DataAccess/Data/ApplicationDbContext.cs
@@ -29,28 +29,28 @@ namespace WebShop.DataAcess.Data
 			base.OnModelCreating(modelBuilder);
 
 			modelBuilder.Entity<Category>().HasData(
-                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
-                new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Category { Id = 1, Name = "Parfume", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Lora", DisplayOrder = 2 },
                 new Category { Id = 3, Name = "History", DisplayOrder = 3 }
                 );
 
             modelBuilder.Entity<Company>().HasData(
-                new Company { Id = 1, Name = "Tech Solution", StreetAddress="123 Tech St", City="Tech City",
-                                PostalCode="12121", State="IL", PhoneNumber="6669990000"},
+                new Company { Id = 1, Name = "Solution", StreetAddress="123", City="City",
+                                PostalCode="12121", State="NY", PhoneNumber="990000"},
                 new Company {
                     Id = 2,
-                    Name = "Vivid Books",
-                    StreetAddress = "999 Vid St",
-                    City = "Vid City",
+                    Name = "Vivid",
+                    StreetAddress = "Vid St",
+                    City = "City",
                     PostalCode = "66666",
                     State = "IL",
-                    PhoneNumber = "7779990000"
+                    PhoneNumber = "770000"
                 },
                 new Company {
                     Id = 3,
-                    Name = "Readers Club",
-                    StreetAddress = "999 Main St",
-                    City = "Lala land",
+                    Name = "Readers",
+                    StreetAddress = "St",
+                    City = "land",
                     PostalCode = "99999",
                     State = "NY",
                     PhoneNumber = "1113335555"
@@ -62,10 +62,10 @@ namespace WebShop.DataAcess.Data
                 new Product
                 {
                     Id = 1,
-                    Title = "Fortune of Time",
-                    Author = "Billy Spark",
-                    Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
-                    ISBN = "SWD9999001",
+                    Title = "Time",
+                    Author = "Billy",
+                    Description = "Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
+                    ISBN = "SWD991",
                     ListPrice = 99,
                     Price = 90,
                     Price50 = 85,
@@ -75,10 +75,10 @@ namespace WebShop.DataAcess.Data
                 new Product
                 {
                     Id = 2,
-                    Title = "Dark Skies",
-                    Author = "Nancy Hoover",
-                    Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
-                    ISBN = "CAW777777701",
+                    Title = "Skies",
+                    Author = "Hoover",
+                    Description = "Shit",
+                    ISBN = "CAd77701",
                     ListPrice = 40,
                     Price = 30,
                     Price50 = 25,
@@ -88,10 +88,10 @@ namespace WebShop.DataAcess.Data
                 new Product
                 {
                     Id = 3,
-                    Title = "Vanish in the Sunset",
-                    Author = "Julian Button",
-                    Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
-                    ISBN = "RITO5555501",
+                    Title = "Sunset",
+                    Author = "Button",
+                    Description = "Praesent vitae sodales libero.",
+                    ISBN = "RITO5501",
                     ListPrice = 55,
                     Price = 50,
                     Price50 = 40,
@@ -101,9 +101,9 @@ namespace WebShop.DataAcess.Data
                 new Product
                 {
                     Id = 4,
-                    Title = "Cotton Candy",
-                    Author = "Abby Muscles",
-                    Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
+                    Title = "Candy",
+                    Author = "Muscles",
+                    Description = "adfsadsadsadsad",
                     ISBN = "WS3333333301",
                     ListPrice = 70,
                     Price = 65,
@@ -114,10 +114,10 @@ namespace WebShop.DataAcess.Data
                 new Product
                 {
                     Id = 5,
-                    Title = "Rock in the Ocean",
-                    Author = "Ron Parker",
-                    Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
-                    ISBN = "SOTJ1111111101",
+                    Title = "Ocean",
+                    Author = "Ron",
+                    Description = "ddasdasdasdd",
+                    ISBN = "SOTJ1101",
                     ListPrice = 30,
                     Price = 27,
                     Price50 = 25,
@@ -127,10 +127,10 @@ namespace WebShop.DataAcess.Data
                 new Product
                 {
                     Id = 6,
-                    Title = "Leaves and Wonders",
-                    Author = "Laura Phantom",
-                    Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
-                    ISBN = "FOT000000001",
+                    Title = "Wonders",
+                    Author = "Phantom",
+                    Description = "131sssssssssss",
+                    ISBN = "FOT00001",
                     ListPrice = 25,
                     Price = 23,
                     Price50 = 22,
